56cdf55d1917795ca78347aba0c166a733fecc17
yeyu98 <763525908@qq.com>
2024-09-12 11:19:17 +0800

    feat: 主题色

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 export default {
   content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
+    colors: {
+      primary: 'var(--primary-color)',
+    },
     extend: {},
   },
   plugins: [],
-};
+}
