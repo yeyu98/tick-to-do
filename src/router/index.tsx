38f00017bb2c49301808b340f2e1a9c86de448d5
yeyu98 <763525908@qq.com>
2024-09-12 21:33:55 +0800

    feat: 重定向

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,13 +2,13 @@
  * @Author: yeyu98
  * @Date: 2024-09-12 16:14:27
  * @LastEditors: yeyu98
- * @LastEditTime: 2024-09-12 17:47:08
+ * @LastEditTime: 2024-09-12 21:33:13
  * @FilePath: \tick-to-do\src\router\index.tsx
  * @Description:
  */
 import { lazy, Suspense } from 'react'
 import FullScreenLoading from '@/components/FullScreenLoading'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, Navigate } from 'react-router-dom'
 
 const SuspenseComponent = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -27,23 +27,27 @@ const Filter = lazy(() => import('@/pages/Filter/Filter'))
 
 const baseRoute = [
   {
-    path: '/',
     element: SuspenseComponent(App),
-    redirect: '/today',
     children: [
       {
-        path: 'today',
-        element: SuspenseComponent(Today),
-        meta: {
-          title: '今天',
-        },
-      },
-      {
-        path: 'filter',
-        element: SuspenseComponent(Filter),
-        meta: {
-          title: '过滤器',
-        },
+        path: '/',
+        element: <Navigate to="/today" />,
+        children: [
+          {
+            path: 'today',
+            element: SuspenseComponent(Today),
+            meta: {
+              title: '今天',
+            },
+          },
+          {
+            path: 'filter',
+            element: SuspenseComponent(Filter),
+            meta: {
+              title: '过滤器',
+            },
+          },
+        ],
       },
     ],
   },
