409fb2d26cd058916b14dd1f0d2a461f542d2ab2
yeyu98 <763525908@qq.com>
2024-09-23 20:17:05 +0800

    chore: 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: yeyu98
+ * @Date: 2024-09-12 21:10:16
+ * @LastEditors: yeyu98
+ * @LastEditTime: 2024-09-23 20:15:42
+ * @Description:
+ */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
@@ -11,5 +18,8 @@ export default defineConfig({
     alias: {
       '@': path.resolve('./src'),
     },
+  },
+  server: {
+    open: true,
   },
 })
