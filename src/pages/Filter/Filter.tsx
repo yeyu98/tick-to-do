cc0112ebbc37f06e409be3b76ac8ec276b38b3e7
yeyu98 <763525908@qq.com>
2024-09-27 15:55:05 +0800

    feat: 过滤器

--- src/pages/Filter/Filter.tsx
+++ src/pages/Filter/Filter.tsx
@@ -2,49 +2,107 @@
  * @Author: yeyu98
  * @Date: 2024-09-12 17:06:38
  * @LastEditors: yeyu98
- * @LastEditTime: 2024-09-27 14:33:18
+ * @LastEditTime: 2024-09-27 15:46:28
  * @FilePath: \tick-to-do\src\pages\Filter\Filter.tsx
  * @Description:
  */
 import { useEffect, useState } from 'react'
-import { Card } from 'antd'
+import { Dropdown, Card, Button, Space, DatePicker } from 'antd'
+import type { MenuProps } from 'antd'
+import { DownOutlined } from '@ant-design/icons'
 import { getTaskLocal } from '@/utils/localData'
 import type { Task } from '@/utils/localData'
-import { isCurrentWeek } from '@/utils/dayjs'
+import dayjs, { isCurrentWeek } from '@/utils/dayjs'
 import styles from './Filter.module.less'
-// 2024年
-// 九月
-// 按最近的月分类，不同的月份可以切换
-// 比如一个月里有四周
-// 每周都使用不同的卡片汇总，卡片之上需要标记
-interface Props {}
 
-const Filter = (props: Props) => {
-  const {} = props
+interface MenuInfo {
+  label: string
+  key: string
+}
+
+const { RangePicker } = DatePicker
+
+const dropdownItems = [
+  {
+    label: '按周过滤',
+    key: 'week',
+  },
+  {
+    label: '按月过滤',
+    key: 'month',
+  },
+  {
+    label: '按季过滤',
+    key: 'quarter',
+  },
+  {
+    label: '按年过滤',
+    key: 'year',
+  },
+  {
+    label: '按日期过滤',
+    key: 'date',
+  },
+]
+
+const Filter = () => {
   const [taskList, setTaskList] = useState<Task[]>([])
-  const initWeekTask = async () => {
+  const [menuInfo, setMenuInfo] = useState<MenuInfo>({ ...dropdownItems[0] })
+
+  const handleMenuClick: MenuProps['onClick'] = (e) => {
+    const currentMenuInfo = dropdownItems.find((item) => item.key == e.key)
+    setMenuInfo(currentMenuInfo!)
+  }
+
+  const menuProps = {
+    items: dropdownItems,
+    onClick: handleMenuClick,
+  }
+
+  const handleChange = async (dates) => {
+    const start = dates[0].$d
+    const end = dates[1].$d
+    getFilteredTask(
+      (item: Task) => dayjs().isBetween(start, end) && item.isFinished,
+    )
+  }
+
+  const getFilteredTask = async (filterCallback: any) => {
     const localTaskList = await getTaskLocal()
     if (localTaskList && localTaskList?.length > 0) {
-      const filterTaskList = localTaskList?.filter(
-        (item) => isCurrentWeek(item.timestamp) && item.isFinished,
-      )
+      const filterTaskList = localTaskList?.filter(filterCallback)
       setTaskList(filterTaskList)
     }
   }
 
   useEffect(() => {
-    initWeekTask()
+    getFilteredTask(
+      (item: Task) => isCurrentWeek(item.timestamp) && item.isFinished,
+    )
   }, [])
 
   return (
     <>
-      <Card title="最近一周" bordered={false} style={{ width: 300 }}>
-        {taskList.map((task) => (
-          <p className={styles['task-item']} key={task.id}>
-            {task.taskContent}
-          </p>
-        ))}
-      </Card>
+      <div className={styles['filter-container']}>
+        <div className="filter-wrapper">
+          <Dropdown menu={menuProps}>
+            <Button>
+              <Space>
+                {menuInfo.label}
+                <DownOutlined />
+              </Space>
+            </Button>
+          </Dropdown>
+          <RangePicker picker={menuInfo.key} onChange={handleChange} />
+        </div>
+        <Card title="本周" bordered={false} style={{ width: 300 }}>
+          {taskList.map((task) => (
+            <div className={styles['task-item']} key={task.id}>
+              {task.taskContent}
+            </div>
+          ))}
+        </Card>
+      </div>
     </>
   )
 }
